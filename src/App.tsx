1ae1e14616171d17d2d6ff27a65e09e0cca66723
Rachel Lewis <60766726+RLewis627@users.noreply.github.com>
2020-04-03 00:26:32 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import Conversation from './components/Conversation';
+import UserLoginBox from './components/UserLoginBox';
+import Register from './components/Register';
+import Thanks from './components/Thanks';
+import "./css/MainStyle.css";
 
 const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
-        </div>
+        <Layout>
+            <Route exact path='/' component={UserLoginBox} />
+            <Route path='/register' component={Register} />
+            <Route path='/talk' component={Conversation} />
+            <Route path='/thankyou' component={Thanks} />
+        </Layout>
     );
 };
 
